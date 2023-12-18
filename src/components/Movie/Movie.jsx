1f09ea1e02fd06de5f38d2073de12512aeed27c1
Bartłomiej Gal <bgal23@bartomiejs-mbp.home>
2023-12-18 21:47:59 +0100

    hwdp

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,21 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getAllVideos } from '../importApi';
 
 const Movie = () => {
   const { movieId } = useParams();
+  const [movieData, setMovieData] = useState('');
 
   useEffect(() => {
     const movieIdUrl = `/movie/${movieId}`;
-    getAllVideos(movieIdUrl).then(movieById => createMovieInfo(movieById.data));
+    getAllVideos(movieIdUrl).then(movieById => {
+      setMovieData(movieById.data);
+    });
   }, [movieId]);
 
-  const createMovieInfo = async data => {
-    //movieInfo = await `<div>${data.title}</div>`;
-    await console.log(data);
-    return;
-  };
-
-  return <div></div>;
+  return (
+    <div>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+        alt=""
+      />
+      <div>
+        <h2>{movieData.title}</h2>
+        <p>User Score: {Math.round(movieData.vote_average * 10)}%</p>
+        <h4>Overview</h4>
+        <p>{movieData.overview}</p>
+        <h4>Genres</h4>
+        <ul>
+          {movieData.genres.map(gen => (
+            <li key={gen.id}>{gen.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default Movie;
