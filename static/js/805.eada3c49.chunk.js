cc81a4548cefab44ac7b57524e0ed57d3571bcb6
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-12-19 20:21:40 +0000

    Deploying to gh-pages from @ BGal23/goit-react-hw-05-movies@a41c169c917757e083dd33e8291589eeb343893f 🚀

---
+++ static/js/805.eada3c49.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("main",{children:"Sorry, this page doesn't exist."})}}}]);
+//# sourceMappingURL=805.eada3c49.chunk.js.map
