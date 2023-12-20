e079dcf2d0cbb8e18fc3cd5834017b6dac818ded
Bartłomiej Gal <bgal23@bartomiejs-mbp.home>
2023-12-20 21:21:38 +0100

    fix

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import styled from 'styled-components';
-import css from './menu-bar.module.css';
+import css from './MenuBar.module.css';
 
 const StyledLink = styled(NavLink)`
   color: black;
